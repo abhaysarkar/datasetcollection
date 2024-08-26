6e281c1b64d22a14120f77fb5edb1745a9993027
abhaysarkar <abhaysheets@gmail.com>
2024-08-26 13:27:41 +0530

    Update FileUpload.jsx

--- src/components/FileUpload.jsx
+++ src/components/FileUpload.jsx
@@ -268,17 +268,17 @@ const FileUpload = ({ email, medicalField, subDepartment, sid, mfid }) => {
   //   }
   // };
 
-  const dataURLtoBlob = (dataURL) => {
-    const arr = dataURL.split(',');
-    const mime = arr[0].match(/:(.*?);/)[1];
-    const bstr = atob(arr[1]);
-    let n = bstr.length;
-    const u8arr = new Uint8Array(n);
-    while (n--) {
-      u8arr[n] = bstr.charCodeAt(n);
-    }
-    return new Blob([u8arr], { type: mime });
-  };
+  // const dataURLtoBlob = (dataURL) => {
+  //   const arr = dataURL.split(',');
+  //   const mime = arr[0].match(/:(.*?);/)[1];
+  //   const bstr = atob(arr[1]);
+  //   let n = bstr.length;
+  //   const u8arr = new Uint8Array(n);
+  //   while (n--) {
+  //     u8arr[n] = bstr.charCodeAt(n);
+  //   }
+  //   return new Blob([u8arr], { type: mime });
+  // };
 
   return (
     <div className="container pt-5">
