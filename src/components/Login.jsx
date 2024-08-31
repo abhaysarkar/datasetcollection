a9567894e2dbf84a6ac7d4f3c451d2a36f05f8f6
abhaysarkar <abhaysheets@gmail.com>
2024-08-31 12:38:21 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -283,13 +283,154 @@
 
 
 
+//Perfect with no loader at otp verifier
 
+// import React, { useState } from 'react';
+// import axios from 'axios';
+// import { useNavigate } from 'react-router-dom';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import { Modal, Button } from 'react-bootstrap';
+// import './Login.css';
+
+// const Login = ({ onVerify }) => {
+//   const [mail, setMail] = useState('');
+//   const [otp, setOtp] = useState('');
+//   const [step, setStep] = useState(1);
+//   const [message, setMessage] = useState('');
+//   const [resendDisabled, setResendDisabled] = useState(true);
+//   const navigate = useNavigate();
+//   const [showModal, setShowModal] = useState(false);
+//   const [loading, setLoading] = useState(false); // Loader state
+
+//   const handleSendOtp = async () => {
+//     try {
+//       setStep(2);
+//       setResendDisabled(true);
+//       await axios.post(`https://datacollection-backend-eb040f587829.herokuapp.com/send-otp/${mail}`);
+//       setMessage('OTP sent successfully.');
+
+//       // Enable resend button after 45 seconds
+//       setTimeout(() => {
+//         setResendDisabled(false);
+//       }, 45000);
+//     } catch (error) {
+//       console.error('Error sending OTP', error);
+//       setMessage('Error sending OTP.');
+//     }
+//   };
+
+//   const handleRedirect = () => {
+//     navigate('*');
+//   };
+
+//   const handleAdminLogin = () => {
+//     navigate('/adminloginpage');
+//   };
+
+//   const handleVerifyOtp = async () => {
+//     setLoading(true);
+//     try {
+//       const response = await axios.post(`https://datacollection-backend-eb040f587829.herokuapp.com/verify-otp/${mail}/${otp}`);
+//       const responseData = response.data;
+//       setMessage(responseData);
+
+//       if (responseData === 'Go To Register Page') {
+//         onVerify();
+//         navigate('/user-form', { state: { email: mail } });
+//       } else if (responseData === 'Go to Main Page') {
+//         setShowModal(true);
+//       }
+//     } catch (error) {
+//       console.error('Error verifying OTP', error);
+//       setMessage('Error verifying OTP.');
+//     }finally {
+//       setLoading(false); // Stop loader
+//     }
+//   };
+
+//   const handleClose = () => setShowModal(false);
+
+//   const handleAudioClick = () => {
+//     navigate('/main', { state: { email: mail } });
+//   };
+
+//   const handleImageClick = () => {
+//     navigate('/image', { state: { email: mail } });
+//   };
+
+//   return (
+//     <div className='login-page'>
+//       <div className="login-container">
+//         <h2>Login with email</h2>
+//         {step === 1 ? (
+//           <>
+//             <input
+//               type="email"
+//               value={mail}
+//               onChange={(e) => setMail(e.target.value)}
+//               placeholder="Enter your email"
+//               className="input-field"
+//             />
+//             <button onClick={handleSendOtp} className="send-otp-button">Send OTP</button>
+//             <button onClick={handleAdminLogin} className='btn btn-warning'>Admin Login</button>
+//           </>
+//         ) : (
+//           <>
+//             <input
+//               type="text"
+//               value={otp}
+//               onChange={(e) => setOtp(e.target.value)}
+//               placeholder="Enter OTP here"
+//               className="input-field"
+//             />
+//             <div className='resend-submit-container'>
+//               <button onClick={handleVerifyOtp} className="submit-button">Submit</button>
+//               <button
+//                 onClick={handleRedirect}
+//                 className="redirect-button"
+//                 disabled={resendDisabled}
+//                 style={{ backgroundColor: resendDisabled ? 'gray' : 'green', cursor: resendDisabled ? 'not-allowed' : 'pointer' }}
+//               >
+//                 Resend
+//               </button>
+//             </div>
+//           </>
+//         )}
+//         <p>{message}</p>
+//       </div>
+//       <Modal show={showModal} onHide={handleClose}>
+//         <Modal.Header closeButton>
+//           <Modal.Title>Select Data Type</Modal.Title>
+//         </Modal.Header>
+//         <Modal.Body>
+//           <p>What type of data do you want to give?</p>
+//         </Modal.Body>
+//         <Modal.Footer>
+//           <Button variant="primary" onClick={handleAudioClick}>
+//             Audio
+//           </Button>
+//           <Button variant="secondary" onClick={handleImageClick}>
+//             Image
+//           </Button>
+//         </Modal.Footer>
+//       </Modal>
+//     </div>
+//   );
+// };
+
+// export default Login
+
+
+
+
+//runs perfectly with loader and set timeout 45 seconds.
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Modal, Button } from 'react-bootstrap';
+import { Modal, Button, Spinner } from 'react-bootstrap';
 import './Login.css';
+
 
 const Login = ({ onVerify }) => {
   const [mail, setMail] = useState('');
@@ -301,12 +442,14 @@ const Login = ({ onVerify }) => {
   const [showModal, setShowModal] = useState(false);
   const [loading, setLoading] = useState(false); // Loader state
 
+
   const handleSendOtp = async () => {
     try {
       setStep(2);
       setResendDisabled(true);
       await axios.post(`https://datacollection-backend-eb040f587829.herokuapp.com/send-otp/${mail}`);
       setMessage('OTP sent successfully.');
+
 
       // Enable resend button after 45 seconds
       setTimeout(() => {
@@ -318,13 +461,16 @@ const Login = ({ onVerify }) => {
     }
   };
 
+
   const handleRedirect = () => {
     navigate('*');
   };
 
+
   const handleAdminLogin = () => {
     navigate('/adminloginpage');
   };
+
 
   const handleVerifyOtp = async () => {
     setLoading(true);
@@ -332,6 +478,7 @@ const Login = ({ onVerify }) => {
       const response = await axios.post(`https://datacollection-backend-eb040f587829.herokuapp.com/verify-otp/${mail}/${otp}`);
       const responseData = response.data;
       setMessage(responseData);
+
 
       if (responseData === 'Go To Register Page') {
         onVerify();
@@ -342,20 +489,24 @@ const Login = ({ onVerify }) => {
     } catch (error) {
       console.error('Error verifying OTP', error);
       setMessage('Error verifying OTP.');
-    }finally {
+    } finally {
       setLoading(false); // Stop loader
     }
   };
 
+
   const handleClose = () => setShowModal(false);
+
 
   const handleAudioClick = () => {
     navigate('/main', { state: { email: mail } });
   };
 
+
   const handleImageClick = () => {
     navigate('/image', { state: { email: mail } });
   };
+
 
   return (
     <div className='login-page'>
@@ -383,7 +534,15 @@ const Login = ({ onVerify }) => {
               className="input-field"
             />
             <div className='resend-submit-container'>
-              <button onClick={handleVerifyOtp} className="submit-button">Submit</button>
+              <button onClick={handleVerifyOtp} className="submit-button" disabled={loading}>
+                {loading ? (
+                  <>
+                    <Spinner animation="border" size="sm" /> Verifying...
+                  </>
+                ) : (
+                  'Submit'
+                )}
+              </button>
               <button
                 onClick={handleRedirect}
                 className="redirect-button"
@@ -417,4 +576,5 @@ const Login = ({ onVerify }) => {
   );
 };
 
-export default Login
+
+export default Login;	
