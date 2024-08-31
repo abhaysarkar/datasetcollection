2dff5d0fa26944946088c69ce25131aef1c64847
abhaysarkar <abhaysheets@gmail.com>
2024-08-31 12:43:28 +0530

    Create Loader.jsx

---
+++ src/components/Loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Loader.css'; // Add styles for the loader
+
+const Loader = () => {
+  return (
+    <div className="loader-container">
+      <div className="loader"></div>
+    </div>
+  );
+};
+
+export default Loader;
