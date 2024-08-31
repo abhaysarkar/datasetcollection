86e78126029307c5101c619a75c090862ef085a4
abhaysarkar <abhaysheets@gmail.com>
2024-08-31 12:41:52 +0530

    Create AudioLoader.jsx

---
+++ src/components/AudioLoader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AudioLoader.css'; // Add styles for the loader
+
+const Loader = () => {
+  return (
+    <div className="loader-container">
+      <div className="loader"></div>
+    </div>
+  );
+};
+
+export default Loader;
